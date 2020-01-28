6d9207e691f0315d1915fd01daafef762c75afee
Sandeep <sk9958814616@gmail.com>
2020-01-28 23:13:16 +0530

    adding index.php file

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="description" content="Portfolio">
+        <meta name="author" content="John Doe">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Sandeep (coderode)</title>
+    </head>
+    <body>
+    <?php
+     echo "hello world welcome to mypage";   
+    ?>
+    </body>
+
+</html>
