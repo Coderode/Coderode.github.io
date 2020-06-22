3759086b7a4d0a4cadd44fd126e41b15cfd0a835
Sandeep <sk9958814616@gmail.com>
2020-06-22 12:59:50 +0530

    education page

--- js/index.js
+++ js/index.js
@@ -1,15 +1,5 @@
-$(document).ready(function(){
-    $(window).scroll(function() { // check if scroll event happened
-      if ($(document).scrollTop() > 600) { // check if user scrolled more than 50 from top of the browser window
-        //$(".navbar").css("background-color", "black"); // if yes, then change the color of class "navbar-fixed-top" to white (#f8f8f8)
-        $(".navbar").removeClass('bg-transparent');
-        $(".navbar").addClass('navbar-custom');
-        $(".navbar-nav").addClass('navbar-text-color');
-      } else {
-        //$(".navbar").css("background-color", "transparent"); // if not, change it back to transparent
-        $(".navbar").removeClass('navbar-custom');
-        $(".navbar").addClass('bg-transparent');
-        $(".navbar-nav").removeClass('navbar-text-color');
-      }
-    });
-  });
+// $( "#about" ).click(function() {
+//   $( ".about-image > img" ).slideUp( "slow", function() {
+//     // Animation complete.
+//   });
+// });
